5b2ef9d4fcead2bac9f0713a05e4b2e4d4319f74
ndzywork998 <zhang.yi@pitayagames.com>
2024-05-10 10:42:30 +0800

    chore: 1

--- docs/article/_react/uuid.tsx
+++ docs/article/_react/uuid.tsx
@@ -9,9 +9,24 @@ function generateUUID() {
   });
 }
 
+function generateRandomString(length: number) {
+  // 定义所有可能字符，包括大小写英文字母和阿拉伯数字
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = ''; // 初始化结果字符串
+
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    // 对每个字符位置生成一个随机数，并添加对应的字符到结果字符串
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
 const { Paragraph } = Typography;
 const Uuid = () => {
   const [uuid, setUuid] = useState('');
+  const [s, setS] = useState('');
 
   return (
     <div>
@@ -19,6 +34,11 @@ const Uuid = () => {
       &nbsp;&nbsp;&nbsp;&nbsp;
       <button onClick={() => setUuid('')}>清除</button>
       {uuid && <Paragraph copyable>{uuid}</Paragraph>}
+      <hr />
+      <button onClick={() => setS(generateRandomString(8))}>生成</button>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button onClick={() => setS('')}>清除</button>
+      {s && <Paragraph copyable>{s}</Paragraph>}
     </div>
   );
 };
